bea8374d3396639901ecd7f96c26ed533e8b23a6
Cole Dahlen <coledahlen@gmail.com>
2023-01-27 13:25:31 -0600

    delete works

--- src/components/Favorites/FavoriteItem.jsx
+++ src/components/Favorites/FavoriteItem.jsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 function FavoriteItem({favorite}){
+    const dispatch = useDispatch();
+    const deleteFavorite = () =>{
+        dispatch({
+            type: 'SAGA/DELETE_FAVORITE',
+            payload: favorite.id
+        })
+    }
     return(
         <>
+         <img src={favorite.url}/>
+         <button onClick={deleteFavorite}>Delete</button>
         </>
     )
 }
